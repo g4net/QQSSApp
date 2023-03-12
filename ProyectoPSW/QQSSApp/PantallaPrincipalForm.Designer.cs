1690b54be95575219a8e871e4b8dd74abd86462b
NickC25C <niccm25@gmail.com>
2023-03-12 12:30:07 +0100

    fotos e interfaz

--- ProyectoPSW/QQSSApp/PantallaPrincipalForm.Designer.cs
+++ ProyectoPSW/QQSSApp/PantallaPrincipalForm.Designer.cs
@@ -31,14 +31,19 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.comenzarButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(233, 39);
+            this.pictureBox1.Image = global::QQSSApp.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(202, -36);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(330, 218);
+            this.pictureBox1.Size = new System.Drawing.Size(390, 309);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -46,9 +51,9 @@
             // 
             this.comenzarButton.BackColor = System.Drawing.Color.SpringGreen;
             this.comenzarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comenzarButton.Location = new System.Drawing.Point(202, 352);
+            this.comenzarButton.Location = new System.Drawing.Point(260, 348);
             this.comenzarButton.Name = "comenzarButton";
-            this.comenzarButton.Size = new System.Drawing.Size(390, 50);
+            this.comenzarButton.Size = new System.Drawing.Size(266, 50);
             this.comenzarButton.TabIndex = 1;
             this.comenzarButton.Text = "COMENZAR";
             this.comenzarButton.UseVisualStyleBackColor = false;
@@ -58,23 +63,47 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(235, 276);
+            this.label1.Location = new System.Drawing.Point(242, 276);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(328, 54);
+            this.label1.Size = new System.Drawing.Size(306, 54);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Odd Quizz (?)";
+            this.label1.Text = "QQSS-Quizz";
             // 
-            // PantallaPrincipal
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(181, -53);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(435, 512);
+            this.textBox1.TabIndex = 26;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QQSSApp.Properties.Resources.agüita;
+            this.pictureBox2.Location = new System.Drawing.Point(1, -5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(801, 464);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
+            // 
+            // PantallaPrincipalForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.comenzarButton);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "PantallaPrincipal";
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "PantallaPrincipalForm";
+            this.Text = "Principal";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +114,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button comenzarButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
