05c11744ca9af8dcb27f22bf809f801f59e88124
NickC25C <niccm25@gmail.com>
2023-03-24 23:35:40 +0100

    nuevo form: partida ganada

---
+++ ProyectoPSW/QQSSApp/PartidaGanada.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace QQSSApp
+{
+    partial class PartidaGanada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.correcto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.puntuacion = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.puntuacion_partida = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.continuar_salir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.puntuacion_total = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.panel1.Controls.Add(this.correcto);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(571, 98);
+            this.panel1.TabIndex = 0;
+            // 
+            // correcto
+            // 
+            this.correcto.AutoSize = true;
+            this.correcto.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.correcto.ForeColor = System.Drawing.Color.White;
+            this.correcto.Location = new System.Drawing.Point(119, 22);
+            this.correcto.Name = "correcto";
+            this.correcto.Size = new System.Drawing.Size(319, 58);
+            this.correcto.TabIndex = 0;
+            this.correcto.Text = "CORRECTO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 91);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "+";
+            // 
+            // puntuacion
+            // 
+            this.puntuacion.AutoSize = true;
+            this.puntuacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntuacion.Location = new System.Drawing.Point(183, 142);
+            this.puntuacion.Name = "puntuacion";
+            this.puntuacion.Size = new System.Drawing.Size(255, 91);
+            this.puntuacion.TabIndex = 2;
+            this.puntuacion.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Puntuación Partida:";
+            // 
+            // puntuacion_partida
+            // 
+            this.puntuacion_partida.AutoSize = true;
+            this.puntuacion_partida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntuacion_partida.Location = new System.Drawing.Point(340, 321);
+            this.puntuacion_partida.Name = "puntuacion_partida";
+            this.puntuacion_partida.Size = new System.Drawing.Size(70, 25);
+            this.puntuacion_partida.TabIndex = 4;
+            this.puntuacion_partida.Text = "label5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.label2.Location = new System.Drawing.Point(100, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(386, 38);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Has ganado la partida!!!";
+            // 
+            // continuar_salir
+            // 
+            this.continuar_salir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.continuar_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.continuar_salir.Location = new System.Drawing.Point(34, 455);
+            this.continuar_salir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.continuar_salir.Name = "continuar_salir";
+            this.continuar_salir.Size = new System.Drawing.Size(501, 52);
+            this.continuar_salir.TabIndex = 7;
+            this.continuar_salir.Text = "CONTINUAR";
+            this.continuar_salir.UseVisualStyleBackColor = false;
+            this.continuar_salir.Click += new System.EventHandler(this.continuar_salir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(136, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Puntuación Total:";
+            // 
+            // puntuacion_total
+            // 
+            this.puntuacion_total.AutoSize = true;
+            this.puntuacion_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntuacion_total.Location = new System.Drawing.Point(340, 375);
+            this.puntuacion_total.Name = "puntuacion_total";
+            this.puntuacion_total.Size = new System.Drawing.Size(70, 25);
+            this.puntuacion_total.TabIndex = 9;
+            this.puntuacion_total.Text = "label5";
+            // 
+            // PartidaGanada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 554);
+            this.Controls.Add(this.puntuacion_total);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.continuar_salir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.puntuacion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.puntuacion_partida);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PartidaGanada";
+            this.Text = "PuntuacionForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label correcto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label puntuacion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label puntuacion_partida;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button continuar_salir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label puntuacion_total;
+    }
+}
---
+++ ProyectoPSW/QQSSApp/PartidaGanada.cs
@@ -0,0 +1,37 @@
+﻿using ProyectoPSWMain.Entities;
+using ProyectoPSWMain.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QQSSApp
+{
+    public partial class PartidaGanada : Form
+    {
+        IQQSSService service;
+        
+        int retoindex;
+        public PartidaGanada(IQQSSService service, int index)
+        {
+            InitializeComponent();
+            this.service = service;
+            
+            retoindex= index;
+        }
+
+        private void continuar_salir_Click(object sender, EventArgs e)
+        {
+            PartidaForm partida = new PartidaForm(service, retoindex);
+            partida.Show();
+            this.Close();
+        }
+
+    }
+}
