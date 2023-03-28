c1837e6ef5a492bdfba736d60f04efd3fd99ee68
NickC25C <niccm25@gmail.com>
2023-03-28 21:55:14 +0200

    form de niveles

---
+++ ProyectoPSW/QQSSApp/Niveles.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace QQSSApp
+{
+    partial class Niveles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BotonNivel0 = new System.Windows.Forms.Button();
+            this.BotonNivel1 = new System.Windows.Forms.Button();
+            this.BotonNivel2 = new System.Windows.Forms.Button();
+            this.BotonNivel3 = new System.Windows.Forms.Button();
+            this.BotonNivel4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QQSSApp.Properties.Resources.agüita;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 455);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.BotonNivel4);
+            this.panel1.Controls.Add(this.BotonNivel3);
+            this.panel1.Controls.Add(this.BotonNivel2);
+            this.panel1.Controls.Add(this.BotonNivel1);
+            this.panel1.Controls.Add(this.BotonNivel0);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(52, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 355);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(276, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIVEL";
+            // 
+            // BotonNivel0
+            // 
+            this.BotonNivel0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.BotonNivel0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonNivel0.ForeColor = System.Drawing.Color.White;
+            this.BotonNivel0.Location = new System.Drawing.Point(90, 72);
+            this.BotonNivel0.Name = "BotonNivel0";
+            this.BotonNivel0.Size = new System.Drawing.Size(75, 75);
+            this.BotonNivel0.TabIndex = 1;
+            this.BotonNivel0.Text = "0";
+            this.BotonNivel0.UseVisualStyleBackColor = false;
+            // 
+            // BotonNivel1
+            // 
+            this.BotonNivel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.BotonNivel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonNivel1.ForeColor = System.Drawing.Color.White;
+            this.BotonNivel1.Location = new System.Drawing.Point(299, 72);
+            this.BotonNivel1.Name = "BotonNivel1";
+            this.BotonNivel1.Size = new System.Drawing.Size(75, 75);
+            this.BotonNivel1.TabIndex = 2;
+            this.BotonNivel1.Text = "1";
+            this.BotonNivel1.UseVisualStyleBackColor = false;
+            // 
+            // BotonNivel2
+            // 
+            this.BotonNivel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.BotonNivel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonNivel2.ForeColor = System.Drawing.Color.White;
+            this.BotonNivel2.Location = new System.Drawing.Point(505, 72);
+            this.BotonNivel2.Name = "BotonNivel2";
+            this.BotonNivel2.Size = new System.Drawing.Size(75, 75);
+            this.BotonNivel2.TabIndex = 3;
+            this.BotonNivel2.Text = "2";
+            this.BotonNivel2.UseVisualStyleBackColor = false;
+            // 
+            // BotonNivel3
+            // 
+            this.BotonNivel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.BotonNivel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonNivel3.ForeColor = System.Drawing.Color.White;
+            this.BotonNivel3.Location = new System.Drawing.Point(187, 198);
+            this.BotonNivel3.Name = "BotonNivel3";
+            this.BotonNivel3.Size = new System.Drawing.Size(75, 75);
+            this.BotonNivel3.TabIndex = 4;
+            this.BotonNivel3.Text = "3";
+            this.BotonNivel3.UseVisualStyleBackColor = false;
+            // 
+            // BotonNivel4
+            // 
+            this.BotonNivel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(221)))), ((int)(((byte)(130)))));
+            this.BotonNivel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonNivel4.ForeColor = System.Drawing.Color.White;
+            this.BotonNivel4.Location = new System.Drawing.Point(409, 198);
+            this.BotonNivel4.Name = "BotonNivel4";
+            this.BotonNivel4.Size = new System.Drawing.Size(75, 75);
+            this.BotonNivel4.TabIndex = 5;
+            this.BotonNivel4.Text = "4";
+            this.BotonNivel4.UseVisualStyleBackColor = false;
+            // 
+            // Niveles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Niveles";
+            this.Text = "Niveles";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BotonNivel4;
+        private System.Windows.Forms.Button BotonNivel3;
+        private System.Windows.Forms.Button BotonNivel2;
+        private System.Windows.Forms.Button BotonNivel1;
+        private System.Windows.Forms.Button BotonNivel0;
+        private System.Windows.Forms.Label label1;
+    }
+}
