03323aec688ef6bca36d456140a485fa59b6d95c
Yavae <98843718+Yavae@users.noreply.github.com>
2023-05-02 01:26:23 +0200

    patron estrategia

---
+++ ProyectoPSW/ProyectoPSWMain/BussinesLogic/Services/Context.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPSWMain.BussinesLogic.Services
+{
+    public class Context
+    {
+        private PuntosStrategy puntosStrategy;
+        private int puntos;
+
+        public Context(PuntosStrategy puntosStrategy) 
+        {
+            this.puntosStrategy = puntosStrategy;
+        }
+
+        public void SetSortStrategy(PuntosStrategy puntosStrategy)
+        {
+            this.puntosStrategy = puntosStrategy;
+        }
+        
+        public void AñadirPuntos()
+        {
+            PuntosStrategy.AñadirPuntos(puntos);
+        }
+    }
+    public abstract class PuntosStrategy
+    {
+        internal static void AñadirPuntos(int puntos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract void AñadirPutos(int puntos);
+    }
+    public class AñadirSinPista : PuntosStrategy 
+    {
+        public override void AñadirPutos(int puntos)
+        {
+            //implementar
+            throw new NotImplementedException();
+        }
+    }
+    public class AñadirConPista : PuntosStrategy 
+    {
+        public override void AñadirPutos(int puntos)
+        {
+            //implementar
+            throw new NotImplementedException();
+        }
+    }
+}
