26b6bd07375c81df179a1f384aa1094b7d10358c
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2023-05-04 20:29:26 +0200

    Tiempo Reducido a 3 segundos

--- ProyectoPSW/QQSSApp/PartidaForm.cs
+++ ProyectoPSW/QQSSApp/PartidaForm.cs
@@ -192,7 +192,7 @@ namespace QQSSApp
             timer2.Interval = 1000;
             tiempoContador = 30;
             timer2.Start();
-            tiempodeMostrarRta = 6;
+            tiempodeMostrarRta = 1;
             
         }
 
